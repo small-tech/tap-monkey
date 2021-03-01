9eeb29bcc9058a48059b9971a2502a57c63b9d31
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-03-01 20:19:28 +0000

    The monkey lives!

--- README.md
+++ README.md
@@ -1,8 +1,45 @@
-# tap-coverage
+# tap-monkey
 
-Very basic tap formatter for coverage output from a tool like c8 and nyc.
+## 🍌️🐒️
 
-## What it does
+A [tap]() formatter that’s also a monkey.
 
-It outputs only errors (if any), and the final coverage report.
+Use it like [tap-spec]() for running regular tests and also like [tap-nyc]() for running coverage with [c8]() or [nyc]().
 
+## Install
+
+```sh
+npm i @small-tech/tap-monkey
+```
+
+## Use
+
+Pipe your tap test output to tap-monkey (e.g., if your _test.js_ file contains [tape]() test):
+
+```sh
+node test.js | npx tap-monkey
+```
+
+Or, as a test task in your _package.json_ file:
+
+```json
+"scripts" : [
+  "test": "node test.js | tap-monkey"
+]
+```
+
+Or, if you have more than one test file:
+
+```json
+"scripts" : [
+  "test": "tape test/**/*js | tap-monkey"
+]
+```
+
+Or, if you want to run code coverage (e.g., using [c8]()):
+
+```json
+"scripts" : [
+  "test": "c8 tape test/**/*js | tap-monkey"
+]
+```
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import ora from 'ora'
+import Ora from 'ora'
+import chalk from 'chalk'
 import { performance } from 'perf_hooks'
 import tapOut from '@small-tech/tap-out'
 
-const spinner = ora('Running tests').start({
-  discardStdin: false,
+let hasFailures = false
+
+const indentedMonkey = {
+  "interval": 300,
+  "frames": [
+    "  🙈 ",
+    "  🙈 ",
+    "  🙉 ",
+    "  🙊 "
+  ]
+}
+
+const spinner = new Ora({
+    spinner: indentedMonkey,
+    discardStdin: false,
+    text: 'Running tests'
 })
 
 const parser = tapOut()
@@ -12,42 +27,87 @@ process.stdin.pipe(parser)
 const startTime = performance.now()
 
 let printingCoverage = false
+let coverageBorderCount = 0
 let currentTest = ''
+let testStatusNoticePrinted = false
 
 // Only print coverage unless there’s an error.
 parser.on('test', test => {
+  spinner.start()
+
   currentTest = test.name
-  spinner.text = currentTest
+  spinner.text = `Running ${chalk.underline(currentTest)} tests`
 })
 
 parser.on('assert', assert => {
-  spinner.text = `${currentTest} (${assert.number})`
+  // spinner.text = `Running ${chalk.underline(currentTest)} tests (${assert.number})`
+})
+
+parser.on('pass', assert => {
+  spinner.text = `${chalk.green('✔')} ${assert.name}`
 })
 
 parser.on('fail', assert => {
+  hasFailures = true
   spinner.stop()
 
-  console.log(`Test #${assert.name} failed.\n`)
-  console.log(assert.raw)
+  const e = assert.error
+
+  console.log(`${chalk.red('✖ FAIL:')} ${assert.name} \n`)
+
+  if (e.operator !== undefined) console.log(`  operator:`, e.operator)
+  if (e.expected !== undefined) console.log(`  ${chalk.green(`expected: ${e.expected}`)}`)
+  if (e.actual !== undefined)   console.log(`  ${chalk.red(`actual  : ${e.actual}`)}`)
+  if (e.at !== undefined)       console.log(`  ${chalk.yellow(`at      : ${e.at}`)}`)
+
+  console.log()
+
+  e.stack.split('\n').forEach(line => {
+    console.log(' ', chalk.gray(line))
+  })
 
   spinner.start()
 })
 
 parser.on('comment', comment => {
+  let commentText = comment.raw
   if (!printingCoverage) {
     printingCoverage = true
-    spinner.succeed('Tests complete.\n\nCoverage:\n')
+    spinner.stop()
   }
-  console.log(comment.raw)
+
+  if (commentText.startsWith('----')) {
+    coverageBorderCount++
+    switch(coverageBorderCount) {
+      case 1: commentText = `╭─${commentText.replace(/\-\|\-/g, '─┬─')}─╮`; break
+      case 2: commentText = `├─${commentText.replace(/\-\|\-/g, '─┼─')}─┤`; break
+      case 3: commentText = `╰─${commentText.replace(/\-\|\-/g, '─┴─')}─╯\n`; break
+      default: throw new Error('Too many borders found in coverage. Panic!')
+    }
+  } else {
+    commentText = `│ ${commentText} │`
+  }
+
+  console.log(commentText.replace(/\|/g, '│').replace(/\-/g, '─'))
 })
 
 parser.on('output', results => {
   const duration = ((performance.now() - startTime)/1000).toFixed(2)
   spinner.stop()
 
+  const total = results.asserts.length
+  const passing = results.pass.length
+  const failing = results.fail.length
+
+  if (hasFailures) {
+    console.log(`  🙊️ ${chalk.magenta('There are failed tests.')}`)
+  } else {
+    console.log(`  🍌️ ${chalk.green('All tests passing!')}`)
+  }
+
   console.log()
-  console.log(`total: ${results.asserts.length}`)
-  console.log(`passing: ${results.pass.length}`)
-  console.log(`failing: ${results.fail.length}`)
-  console.log(`duration: ${duration} secs`)
+  console.log(            `  Total     ${total}`)
+  console.log(chalk.green(`  Passing   ${passing}`))
+  console.log(chalk.red(  `  Failing   ${failing}`))
+  console.log(chalk.gray( `  Duration  ${duration} secs`))
 })
